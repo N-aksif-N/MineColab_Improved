4bd364ae99e1092e3d4ec6183daffaaff6b501c2
Aksif <162333410+N-aksif-N@users.noreply.github.com>
2024-07-31 11:03:27 +0700

    Update create_page_1.py

--- streamlit-app/frontends/create_page_1.py
+++ streamlit-app/frontends/create_page_1.py
@@ -9,7 +9,9 @@ del uid['Sec-Websocket-Key']
 uid = str(uid)
 for user_ in user['user']:
   if user_ != 'authtoken':
-    if uid in user['user'][user_]['user_id']: user_name = user_; break
+    if uid in user['user'][user_]['user_id']: 
+      if user['user'][user_]['server_in_use'] != '': st.switch_page(st.Page('frontends/main_page.py'))
+      user_name = user_; break
 if user_name == '': st.switch_page(st.Page('frontends/login.py'))
   
 drive_dir = listdir(drive_path); drive_dir.remove('serverconfig.txt');  drive_dir.remove('logs');
@@ -19,13 +21,14 @@ col1, col2, col3 = st.columns(3, vertical_alignment="bottom")
 with col1:
   if st.button('Create', use_container_width= True):
     if server_name != '' and tunnel_service != None:
-      SET_SERVERCONFIG(tunnel_service, server_name)
-      Install_server(server_name = server_name, server_type  = "vanilla" , version = 'vanilla - latest_version', tunnel_service = tunnel_service, server_config=False)
-      PROGRESS()
-      user['user'][user_name]['server_in_use'] = server_name
-      st.session_state['Starting'][server_name] = ['Start', False]
-      sleep(1)
-      st.switch_page(st.Page('frontends/main_page.py'))
+      if server_name not in drive_dir:
+        SET_SERVERCONFIG(tunnel_service, server_name)
+        Install_server(server_name = server_name, server_type  = "vanilla" , version = 'vanilla - latest_version', tunnel_service = tunnel_service)
+        user['user'][user_name]['server_in_use'] = server_name
+        st.session_state['Starting'][server_name] = ['Start', False]
+        PROGRESS()
+        st.switch_page(st.Page('frontends/main_page.py'))
+      else: ERROR('You have created that server. Please give a different minecraft server name.')
 with col2:
   if st.button('Exit to login page', use_container_width= True):
     st.switch_page(st.Page('frontends/choose_server.py'))
