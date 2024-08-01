9eba86e074fbe1f0ec11a75b34e79fd440e583f3
Aksif <162333410+N-aksif-N@users.noreply.github.com>
2024-08-01 11:57:55 +0700

    Create google-colab-auto-update.py

---
+++ google-colab-auto-update.py
@@ -0,0 +1,36 @@
+from shutil import rmtree
+from os.path import exists, join
+from os import makedirs, listdir, chdir
+
+chdir('/')
+path = '/content/drive'
+drive_path = join(path, 'MyDrive/minecraft_server')
+
+chdir(f'{path}/content')
+r = get('https://raw.githubusercontent.com/N-aksif-N/MineColab_Improved/app/streamlit-app/update.txt')
+if 'update=True' in r.text or exists(join(path, 'MyDrive/streamlit-app')) == False:
+  if exists(join(path, 'MyDrive/streamlit-app')):
+    rmtree(join(path, 'MyDrive/streamlit-app'))
+    sleep(5)
+  dict = {'app.py':                    'https://raw.githubusercontent.com/N-aksif-N/MineColab_Improved/app/streamlit-app/app.py',
+         'backends/settings.py':       'https://raw.githubusercontent.com/N-aksif-N/MineColab_Improved/app/streamlit-app/backends/settings.py',
+         'frontends/choose_server.py': 'https://raw.githubusercontent.com/N-aksif-N/MineColab_Improved/app/streamlit-app/frontends/choose_server.py',
+         'frontends/create_page_1.py': 'https://raw.githubusercontent.com/N-aksif-N/MineColab_Improved/app/streamlit-app/frontends/create_page_1.py',
+         'frontends/login.py':         'https://raw.githubusercontent.com/N-aksif-N/MineColab_Improved/app/streamlit-app/frontends/login.py',
+         'frontends/main_page.py':     'https://raw.githubusercontent.com/N-aksif-N/MineColab_Improved/app/streamlit-app/frontends/main_page.py'}
+  makedirs('MyDrive/streamlit-app')
+  makedirs('MyDrive/streamlit-app/backends')
+  makedirs('MyDrive/streamlit-app/frontends')
+  for key in dict:
+    with open(f'MyDrive/streamlit-app/{key}', 'w') as f:
+      r = get(dict[key])
+      f.write(r.text)
+  dump({'choose': True, 'user': {'authtoken': ''}}, open(join(path, 'MyDrive/streamlit-app/user.txt'), 'w'))
+  # Creating minecraft_server folder
+  if exists(drive_path) == False:
+    makedirs(drive_path)
+    makedirs(f'{drive_path}/logs')
+  if exists(join(path, drive_path, 'serverconfig.txt')) == False:
+    dump({"server_list": [], "ngrok_proxy" : {"authtoken" : '', "region" : ''}, "zrok_proxy": {"authtoken": ''}, "localtonet_proxy": {"authtoken": ''}}, open(SERVERCONFIG, 'w'))
+sleep(40)
+chdir(drive_path)
