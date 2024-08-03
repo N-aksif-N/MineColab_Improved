f7ccc8a2365eaf70334f422623c8ee4bd89299b9
Aksif <162333410+N-aksif-N@users.noreply.github.com>
2024-08-03 16:29:51 +0700

    Update google-colab-auto-update.py

--- google-colab-auto-update.py
+++ google-colab-auto-update.py
@@ -20,7 +20,7 @@ if 'update=True' in r.text or exists(path + '/MyDrive/streamlit-app') == False:
   with ZipFile('/content/app.zip', 'r') as zip_ref:
     name = zip_ref.namelist()[0]
     zip_ref.extract(f'{name}/streamlit-app', path=f'/content')
-  move(f'/content/{name}/streamlit-app', drive_path)
+  move(f'/content/{name}/streamlit-app', f'/content/drive/MyDrive')
   sleep(10)
   remove('/content/app.zip')
   dump({'choose': True, 'user': {'authtoken': ''}}, open(path + '/MyDrive/streamlit-app/user.txt', 'w'))
